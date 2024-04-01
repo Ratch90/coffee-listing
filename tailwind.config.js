04a9e75953e82061b32f62241f9db241601b2a37
Rasciel Villegas <rak.alex90@gmail.com>
2024-04-01 23:07:56 +0200

    feat(tailwind-config): add project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                  'eerie-black': '#111315',
+                  'eerie-black-light': '#1B1D1F',
+                  'nickel': '#6F757C',
+                  'aero-blue': '#BEE3CC'
+                },
+                secondary: {
+                  'floral-white': '#FEF7EE',
+                  'maximum-yellow-red': '#F6C768',
+                  'terracotta': '#ED735D',
+                }
+            }
+        }
+    },
+    plugins: []
+};
