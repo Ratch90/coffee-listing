b4a3b6b81984a4987f758b014aae6f58a48e35d1
Rasciel Villegas <rak.alex90@gmail.com>
2024-04-26 14:51:40 +0200

    refactor(main.ts): move appConfig to an external file in order to improve folder and files organization

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-
-const modules = [HttpClientModule];
-
-const appConfig: ApplicationConfig = {
-    providers: [importProvidersFrom(...modules)]
-};
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
