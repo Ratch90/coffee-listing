137cd5c35c6cca583b304e0b06c38ec14d5b7fa5
Rasciel Villegas <rak.alex90@gmail.com>
2024-04-26 19:34:30 +0200

    style(coffee-card.component): embeed popular tag into card background image

--- src/app/components/ui/coffee-card/coffee-card.component.ts
+++ src/app/components/ui/coffee-card/coffee-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Coffee } from '../../../interfaces/coffee.interface';
+import { NgStyle } from '@angular/common';
 
 @Component({
     selector: 'app-coffee-card',
     templateUrl: './coffee-card.component.html',
-    standalone: true
+    standalone: true,
+    imports: [NgStyle]
 })
 export class CoffeeCardComponent {
     coffee = input.required<Coffee>();
