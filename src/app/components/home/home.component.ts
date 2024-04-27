0db16ae4a43974e862c3fa219936a0cf33abd48b
Rasciel Villegas <rak.alex90@gmail.com>
2024-04-27 17:50:19 +0200

    refactor(home.component): add header and main content component to the home component
    improving the layout of the project

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,11 @@
-import { Component, computed, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { CoffeeShopInfoComponent, CoffeesListComponent, TabsComponent } from '..';
-import { CoffeeListingService } from '../../services/coffee-listing.service';
+import { Component } from '@angular/core';
 
-import { Coffee } from '../../interfaces/coffee.interface';
+import { HeaderComponent, MainContentComponent } from '../layout';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     standalone: true,
-    imports: [CoffeeShopInfoComponent, TabsComponent, CoffeesListComponent]
+    imports: [HeaderComponent, MainContentComponent]
 })
-export class HomeComponent {
-    private coffeeListingService = inject(CoffeeListingService);
-
-    coffeesListState = this.coffeeListingService.coffeesListState;
-    allCoffees = toSignal<Coffee[], Coffee[]>(this.coffeeListingService.fetchAllCoffees(), { initialValue: [] });
-    availableCoffees = computed(() => this.allCoffees().filter((coffee) => coffee.available));
-}
+export class HomeComponent {}
